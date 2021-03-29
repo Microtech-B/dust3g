d9383206844279598a46228184dd6caf15410748
T <34987384+Penny-T-Lab@users.noreply.github.com>
2021-03-29 11:54:13 +0700

    Add PM_25

---
+++ lib/PM_25/src/lv_PM_25.h
@@ -0,0 +1,38 @@
+/**
+ * @file lv_PM_25.h
+ *
+ */
+
+#ifndef LV_PM_25_H
+#define LV_PM_25_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_PM_25(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_PM_25_H*/
